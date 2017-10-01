e37bbac76fdefce4bc108702af6f89e88bc9f218
Gerard Chin <gerardchin91@gmail.com>
2017-10-01 22:22:27 +1300

    Complete tests for getConfigObj

--- tests/getConfigObj.test.js
+++ tests/getConfigObj.test.js
@@ -4,10 +4,19 @@ import test from "ava";
 
 import getConfigObj from "../lib/getConfigObj";
 
+const validConfig = resolve(__dirname, "assets", "valid_config.yml");
+const invalidConfig = resolve(__dirname, "assets", "invalid_config.yml");
+
 test("successfully parses with the added custom types", async t => {
-  t.true(false);
+  process.env.GPCL = "test";
+  const rootDir = "sleepless";
+  const configObj = getConfigObj(validConfig, rootDir);
+  t.is(configObj.Env, "test");
+  t.true(configObj.Path.startsWith(rootDir));
 });
 
 test("throws an error if invalid syntax", async t => {
-  t.true(true);
+  t.throws(() => {
+    getConfigObj(invalidConfig, "");
+  }, /^YAMLException/);
 });
