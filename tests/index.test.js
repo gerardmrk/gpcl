1aff59030dd1a62cfe17cc2f38fd4a1dffdfdf8e
Gerard Chin <gerardchin91@gmail.com>
2017-10-01 22:38:47 +1300

    Prepare tests for entrypoint

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+
+import test from "ava";
+
+import { loadConfigSync } from "../lib";
+
+test("successfully loads config with both params specified", async t => {
+  t.true(true);
+});
+
+test("correctly infers root directory when not specified", async t => {
+  t.true(true);
+});
+
+test("correctly infers config path when not specified but root dir specified", async t => {
+  t.true(true);
+});
